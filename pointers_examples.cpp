1fee7eca44c5b05a8d2c2f0e730a8ff676a2b7de
Venugopal Ranganathan <95169245+V-Rang@users.noreply.github.com>
2023-05-29 11:51:38 -0500

    examples for "notes_on_pointers"

--- pointers_examples.cpp
+++ pointers_examples.cpp
@@ -1,82 +1,39 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
 
-int main()
+int  main()
 {
 
-    //1. 
-    int *p = new int(4); // new returns a pointer
-    // (*p) = 12; // or p[0]
-    // *(p+1) = 14; // or p[1] ...
-    for(int i=0;i<4;i++)
-    {
-        cout << p[i] << " "; // or cout << *(p+i) << " ";
-    }
-    cout << endl;
+    //1.
+    printf("Case 1\n");
+    int *p = new int(4);
+    p[0] = 14;
+    p[1] = 15;
+    p[2] = 16;
+    p[3] = 17;
 
-    //2. 
-    vector<int>*q = new vector<int>{1,2,3,4};
     for(int i=0;i<4;i++)
     {
-        // cout << *(q+i) << " "; // gives error
-        // cout << q[i] << " "; // gives error
-        cout << (*q)[i] << " "; //works
+        cout << p[i] << " " << *(p+i) << endl;
+        // *(p+i) = i+5;
     }
-    cout << endl;
+
+    //2.
+    printf("Case 2\n");
+    vector<int> *q = new vector<int>{1,2,3,4};
+    for(int i=0;i<4;i++)
+    {
+        cout << (*q)[i] << endl;
+    }
 
     //3. 
-    // vector<int*>r = vector<int>{1,2,3,4}; // wrong
-    // vector<int*>r = vector<int*>{1,2,3,4}; // wrong 
-    // for(int i=0;i<4;i++)
-    // {
-    //     cout << *(r[i]) << " ";
-    // }
-    // cout << endl;
-
-    // int *t = new int(5);
-    // for(int i=0;i<5;i++)
-    // {
-    //     *(t+i) = i+5; //test case , or t[i] = i+5;
-    // }
-
-    // vector<int*>r = vector<int*>{t,t+1,t+2,t+3,t+4}; //works, each element of vector<int*> has to be a pointer to an integer.
-    // for(int i=0;i<5;i++) 
-    // {
-    //     cout << *( (*r)[i]  ) << " ";
-    // }
-    // cout << endl;
-
-
-    // vector<int>*y = new vector<int>{12,13,14,15};
-    // for(int i=0;i<4;i++)
-    // {
-    //     cout << (*y)[i] << " ";
-    // }
-    // cout << endl;
-
-    int*a = new int(4);
+    printf("Case 3\n");
+    vector<int*>r = {p,p+1,p+2,p+3};
     for(int i=0;i<4;i++)
     {
-        a[i] = i+10;
+        cout << *(r[i]) << endl;
     }
-
-    vector<int*>b = {a,a+1,a+2,a+3};
-    printf("Test values\n");
-    for(int i=0;i<4;i++)
-    {
-        cout << a[i] << " " << *(a+i) << endl;
-    }
-
-    printf("Using b\n");
-    for(int i=0;i<4;i++)
-    {
-        cout << *( b[i] ) << endl; //works
-    }
-    // cout << b[0] <<  endl;
-
-
 
 
 
